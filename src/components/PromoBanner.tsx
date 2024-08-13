0beaa02939adcbff9ae4a0ddc8ad2337b2a8cb5a
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-13 23:42:57 +0100

    feat: create promo banner

---
+++ src/components/PromoBanner.tsx
@@ -0,0 +1,11 @@
+const PromoBanner = () => {
+  return (
+    <div className="bg-black flex justify-center items-center w-full text-white h-12">
+      <span className="text-xs font-normal">
+        Sign up and get 20% off for all new arrivals collections
+      </span>
+    </div>
+  );
+};
+
+export default PromoBanner;
