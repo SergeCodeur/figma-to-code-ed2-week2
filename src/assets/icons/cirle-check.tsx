1d499bf3ab949f446006431904cf262f20e90a60
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-13 23:37:18 +0100

    feat: add circle-check icon

---
+++ src/assets/icons/cirle-check.tsx
@@ -0,0 +1,29 @@
+import { SVGProps } from "react";
+const CircleCheck = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={43}
+    height={43}
+    fill="none"
+    {...props}
+  >
+    <rect width={39} height={39} x={2} y={1.5} fill="#fff" rx={20} />
+    <rect
+      width={39}
+      height={39}
+      x={2}
+      y={1.5}
+      stroke="#1D1D1D"
+      strokeWidth={3}
+      rx={20}
+    />
+    <path
+      stroke="#1D1D1D"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3}
+      d="m29.5 15-11 12-5-5.454"
+    />
+  </svg>
+);
+export default CircleCheck;
