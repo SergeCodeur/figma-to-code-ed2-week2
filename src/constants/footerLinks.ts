54141c24e6e4180c8b0bbfa8cf9df625702fbe7e
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-13 23:48:05 +0100

    feat: create header and footer constants

---
+++ src/constants/footerLinks.ts
@@ -0,0 +1,27 @@
+export type FooterLink = {
+  label: string;
+  href: string;
+};
+
+export const productLinks: FooterLink[] = [
+  { label: "Jacket", href: "#" },
+  { label: "T-Shirt", href: "#" },
+  { label: "Jacket", href: "#" },
+  { label: "Shoes", href: "#" },
+  { label: "Sunglasses", href: "#" },
+];
+
+export const categoriesLinks: FooterLink[] = [
+  { label: "Man", href: "#" },
+  { label: "Women", href: "#" },
+  { label: "Kids", href: "#" },
+  { label: "Gift", href: "#" },
+  { label: "New arrival", href: "#" },
+];
+
+export const socialsLinks: FooterLink[] = [
+  { label: "Instagram", href: "#" },
+  { label: "Facebook", href: "#" },
+  { label: "Youtube", href: "#" },
+  { label: "X", href: "#" },
+];
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,16 @@
+export type NavLink = {
+  label: string;
+  href: string;
+};
+
+export const navLinksLeft: NavLink[] = [
+  { label: "Men", href: "#" },
+  { label: "Women", href: "#" },
+  { label: "Kids", href: "#" },
+  { label: "Collection", href: "#" },
+];
+
+export const navLinksRight: NavLink[] = [
+  { label: "Shop", href: "#" },
+  { label: "About us", href: "#" },
+];
