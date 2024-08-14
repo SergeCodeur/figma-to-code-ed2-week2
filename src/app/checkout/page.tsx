a7ebdfad1e0a6fcf4410d6e2236194b7bfb4a550
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-14 01:51:27 +0100

    feat: list produts from the store

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -4,20 +4,50 @@ import { SummaryItem } from "@/components/products/summary-items";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import ShippingMethodCard from "@/components/ui/shipping-method-card";
+import { useCartStore } from "@/store/cartStore";
+import useShippingStore from "@/store/useShippingMethode";
 import Image from "next/image";
 import Link from "next/link";
 
 const CheckOut = () => {
+  const cart = useCartStore((state) => state.cart);
+  const subtotal = useCartStore((state) => state.getSubtotal());
+  const discount = useCartStore((state) => state.getDiscount());
+  const selectedShipping = useShippingStore((state) => state.selectedShipping);
+  const setSelectedShipping = useShippingStore(
+    (state) => state.setSelectedShipping
+  );
+  const getCheckoutOrderTotal = useCartStore(
+    (state) => state.getCheckoutOrderTotal
+  );
+
+  const shippingMethods = [
+    { method: "Free Shipping", details: "7-30 business days", price: 0 },
+    { method: "Regular shipping", details: "3-14 business days", price: 7.5 },
+    { method: "Express shipping", details: "1-3 business days", price: 22.5 },
+  ];
+
+  const selectedShippingMethod = shippingMethods.find(
+    (method) => method.method === selectedShipping
+  );
+
+  const shippingCost = selectedShippingMethod
+    ? selectedShippingMethod.price
+    : 0;
+  const orderTotal = getCheckoutOrderTotal(shippingCost);
+
+  const handleSelectShippingMethod = (method: string) => {
+    setSelectedShipping(method);
+  };
+
   return (
     <section className="container pt-[62px] pb-[104px] space-y-5">
       <h2 className="text-lg md:text-2xl font-semibold font-chillax">
         Checkout
       </h2>
       <div className="flex w-full gap-28 max-lg:flex-col max-lg:gap-[62px]">
-        {/* order */}
         <div className="lg:max-w-[600px] w-full">
-          {/* order */}
-
+          {/* order list */}
           <h3 className="text-sm font-semibold">Your Order</h3>
           <p className="text-xs text-dark-gray mt-1 mb-4">
             By placing your order, you agree to Ballamas{" "}
@@ -30,72 +60,38 @@ const CheckOut = () => {
             </Link>
           </p>
           <ul className="space-y-3">
-            <li className="flex justify-between items-center">
-              <div className="flex gap-2.5">
-                <Image
-                  className="aspect-square rounded-lg"
-                  src="https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenTshirt01.jpg?v=1675455410"
-                  alt="t-shirt"
-                  width={72}
-                  height={72}
-                />
-                <div className="flex flex-col justify-center">
+            {Object.keys(cart).map((productTitle) => {
+              const product = cart[productTitle];
+              const totalPrice =
+                parseFloat(product.price.amount) * product.quantity;
+              return (
+                <li
+                  key={productTitle}
+                  className="flex justify-between items-center"
+                >
+                  <div className="flex gap-2.5">
+                    <Image
+                      className="aspect-square rounded-lg"
+                      src={product.image}
+                      alt={product.title}
+                      width={72}
+                      height={72}
+                    />
+                    <div className="flex flex-col justify-center">
+                      <span className="md:text-sm text-xs-custom font-semibold">
+                        {product.title}
+                      </span>
+                      <span className="md:text-sm text-xs-custom font-medium text-dark-gray">
+                        Color: Green - Size: Large
+                      </span>
+                    </div>
+                  </div>
                   <span className="md:text-sm text-xs-custom font-semibold">
-                    T-shirt
+                    {product.price.currencyCode} {totalPrice.toFixed(2)}
                   </span>
-                  <span className="md:text-sm text-xs-custom font-medium text-dark-gray">
-                    Color: Green - Size: Large
-                  </span>
-                </div>
-              </div>
-              <span className="md:text-sm text-xs-custom font-semibold">
-                $87
-              </span>
-            </li>
-            <li className="flex justify-between items-center">
-              <div className="flex gap-2.5">
-                <Image
-                  className="aspect-square rounded-lg"
-                  src="https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenTshirt01.jpg?v=1675455410"
-                  alt="t-shirt"
-                  width={72}
-                  height={72}
-                />
-                <div className="flex flex-col justify-center">
-                  <span className="md:text-sm text-xs-custom font-semibold">
-                    T-shirt
-                  </span>
-                  <span className="md:text-sm text-xs-custom font-medium text-dark-gray">
-                    Color: Green - Size: Large
-                  </span>
-                </div>
-              </div>
-              <span className="md:text-sm text-xs-custom font-semibold">
-                $87
-              </span>
-            </li>
-            <li className="flex justify-between items-center">
-              <div className="flex gap-2.5">
-                <Image
-                  className="aspect-square rounded-lg"
-                  src="https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenTshirt01.jpg?v=1675455410"
-                  alt="t-shirt"
-                  width={72}
-                  height={72}
-                />
-                <div className="flex flex-col justify-center">
-                  <span className="md:text-sm text-xs-custom font-semibold">
-                    T-shirt
-                  </span>
-                  <span className="md:text-sm text-xs-custom font-medium text-dark-gray">
-                    Color: Green - Size: Large
-                  </span>
-                </div>
-              </div>
-              <span className="md:text-sm text-xs-custom font-semibold">
-                $87
-              </span>
-            </li>
+                </li>
+              );
+            })}
           </ul>
 
           {/* discount form */}
@@ -121,38 +117,36 @@ const CheckOut = () => {
               to get better offer
             </span>
           </form>
-          <SummaryItem label="Subtotal" value="$524.00" />
-          <SummaryItem label="Discount" value="$0" />
+          <SummaryItem label="Subtotal" value={`$${subtotal.toFixed(2)}`} />
+          <SummaryItem label="Discount" value={`$${discount.toFixed(2)}`} />
 
           {/* divide */}
           <span className="block h-[1px] w-full bg-light-gray my-3"></span>
 
           {/* total */}
-          <SummaryItem label="Order total" value="$524.00" isTotal />
+          <SummaryItem
+            label="Order total"
+            value={`$${orderTotal.toFixed(2)}`}
+            isTotal
+          />
 
           {/* chipping method */}
           <div className="w-full mt-3.5 space-y-3">
-            <ShippingMethodCard
-              method="Free Shipping"
-              details="7-30 business days"
-              price="$0"
-            />
-            <ShippingMethodCard
-              method="Regular shipping"
-              details="3-14 business days"
-              price="$7,50"
-            />
-            <ShippingMethodCard
-              method="Express shipping"
-              details="1-3 business days"
-              price="$22,50"
-            />
+            {shippingMethods.map((method) => (
+              <ShippingMethodCard
+                key={method.method}
+                method={method.method}
+                details={method.details}
+                price={`$${method.price.toFixed(2)}`}
+                onSelect={() => handleSelectShippingMethod(method.method)}
+              />
+            ))}
           </div>
         </div>
 
         {/* checkout form */}
         <div className="lg:max-w-[488px] w-full">
-          <CheckoutForm />
+          <CheckoutForm totalPrice={orderTotal} />
         </div>
       </div>
     </section>
