b9c304fdb72e1651148129a02b2527039b232ae9
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-14 18:53:31 +0100

    refactor: cleaning product layout page

--- src/app/products/[productSlug]/page.tsx
+++ src/app/products/[productSlug]/page.tsx
@@ -1,59 +1,32 @@
 "use client";
 import { ProductDetailCard } from "@/components/products/product-detail-card";
 import ProductRecommendations from "@/components/products/product-recommendations";
-import { useProductStore } from "@/store/productStore";
-import { fetchCollections } from "@/utils/fetchCollections";
-import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import useProductDetail from "@/hooks/useProductDetail";
+import { sizeMapping } from "@/utils/sizeMapping";
 
 const ProductDetail = () => {
-  const { productSlug } = useParams();
-  const { setProduct, product } = useProductStore();
+  const { product } = useProductDetail();
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const collections = await fetchCollections();
-      const allProducts = collections.flatMap(
-        (collection: { node: { products: { edges: any[] } } }) =>
-          collection.node.products.edges.map((edge: { node: any }) => edge.node)
-      );
-
-      const foundProduct = allProducts.find(
-        (p: { title: string }) =>
-          p.title.toLowerCase().replace(/ /g, "-") === productSlug
-      );
-
-      setProduct(foundProduct || null);
-    };
-
-    if (productSlug) fetchProduct();
-  }, [productSlug, setProduct]);
-
-  if (!product) return <p>Loading...</p>;
-
-  // Extract color and size options
   const colorOptions = new Set<string>();
   const sizeOptions = new Set<string>();
-  const imagesByColor: { [key: string]: string } = {};
 
-  product.variants.edges.forEach((edge) => {
+  product?.variants?.edges.forEach((edge) => {
     edge.node.selectedOptions.forEach((option) => {
       if (option.name === "Color") {
         colorOptions.add(option.value);
-        if (!imagesByColor[option.value]) {
-          imagesByColor[option.value] = product.images.edges[0]?.node.url || ""; // Utiliser la première image par défaut
-        }
       } else if (option.name === "Size") {
-        sizeOptions.add(option.value);
+        sizeOptions.add(sizeMapping(option.value));
       }
     });
   });
+
+  if (!product) return <p>Loading...</p>;
 
   return (
     <section className="container pt-[76px] space-y-[72px] pb-24">
       <div>
         <ProductDetailCard
-          images={imagesByColor}
+          images={product.images.edges.map((edge) => edge.node.url)}
           title={product.title}
           description={product.description}
           price={{
