fcc46f26c51e71501b7b4dc67a8025c7b013f93f
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-19 14:48:02 +0100

    refactor(product): Fetch recommendations using hook

--- src/components/products/product-recommendations.tsx
+++ src/components/products/product-recommendations.tsx
@@ -1,80 +1,45 @@
 "use client";
 
+import useFetchRecommendations from "@/hooks/use-fetch-recommandations";
 import { ProductCard } from "./product-card";
 
-const product = [
-  {
-    id: 1,
-    image:
-      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenTshirt01.jpg?v=1675455410",
-    title: "SUMMER SHIRT",
-    price: {
-      currencyCode: "$",
-      amount: "99",
-    },
-  },
-  {
-    id: 2,
-    image:
-      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenTshirt01.jpg?v=1675455410",
-    title: "SUMMER SHIRT",
-    price: {
-      currencyCode: "$",
-      amount: "99",
-    },
-  },
-  {
-    id: 3,
-    image:
-      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenSweatpants01.jpg?v=1675455387",
-    title: "SUMMER SHIRT",
-    price: {
-      currencyCode: "$",
-      amount: "99",
-    },
-  },
-  {
-    id: 4,
-    image:
-      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenSweatpants01.jpg?v=1675455387",
-    title: "SUMMER SHIRT",
-    price: {
-      currencyCode: "$",
-      amount: "99",
-    },
-  },
-  {
-    id: 5,
-    image:
-      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenSweatpants01.jpg?v=1675455387",
-    title: "SUMMER SHIRT",
-    price: {
-      currencyCode: "$",
-      amount: "99",
-    },
-  },
-  {
-    id: 6,
-    image:
-      "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenSweatpants01.jpg?v=1675455387",
-    title: "SUMMER SHIRT",
-    price: {
-      currencyCode: "$",
-      amount: "99",
-    },
-  },
-];
-
 const ProductRecommendations = () => {
+  const { recommendations, loading, error } = useFetchRecommendations();
+
+  if (loading) {
+    return (
+      <div className="h-[404px] w-full flex justify-center items-center">
+        Loading...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="h-[404px] w-full flex justify-center items-center">
+        Error: {error.message}
+      </div>
+    );
+  }
   return (
     <div className="space-y-8">
       <h2 className="text-2xl leading-7 md:text-3xl font-semibold font-chillax">
         You may also like
       </h2>
       <ul className="flex gap-3.5 overflow-x-scroll no-scrollbar">
-        {product.map((product, index) => (
+        {recommendations.map((product, index) => (
           <li key={index}>
-            <ProductCard className="min-w-[322px]" {...product} />
+            <ProductCard
+              className="min-w-[322px]"
+              key={product.node.title}
+              image={product.node.images.edges[0]?.node.url}
+              title={product.node.title}
+              price={{
+                currencyCode:
+                  product.node.variants.edges[0]?.node.price.currencyCode,
+                amount: product.node.variants.edges[0]?.node.price.amount,
+              }}
+            />
           </li>
         ))}
       </ul>
